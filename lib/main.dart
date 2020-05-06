1e84e8eb3c40ab85993140a5e0c9861efeaadc0f
toladele <tommy.oladele@mail.utoronto.ca>
2020-05-06 19:36:05 -0400

    Updated cards, styling

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
-                radius: 65,
+                radius: 75,
                 backgroundImage: AssetImage('images/icon3.png'),
               ),
               Text(
@@ -38,6 +38,74 @@ class MyApp extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                     letterSpacing: 2.5),
               ),
+              SizedBox(
+                height: 20.0,
+                width: 200.0,
+                child: Divider(
+                  thickness: 2.0,
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: EdgeInsets.all(4.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.school,
+                      color: Colors.pink.shade300,
+                    ),
+                    title: Text(
+                      'Wilfrid Laurier University',
+                      style: TextStyle(
+                        color: Colors.teal,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 22.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: EdgeInsets.all(4.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.lightbulb_outline,
+                      color: Colors.pink.shade300,
+                    ),
+                    title: Text(
+                      'Computer Science & French',
+                      style: TextStyle(
+                        color: Colors.teal,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 22.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: EdgeInsets.all(4.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.pink.shade300,
+                    ),
+                    title: Text(
+                      'to@tomiwaola.ca',
+                      style: TextStyle(
+                        color: Colors.teal,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 22.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
@@ -45,3 +113,23 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+//Row(
+//children: <Widget>[
+//Icon(
+//Icons.email,
+//color: Colors.pink.shade300,
+//),
+//SizedBox(
+//width: 20.0,
+//),
+//Text(
+//'to@tomiwaola.ca',
+//style: TextStyle(
+//color: Colors.teal,
+//fontFamily: 'SourceSansPro',
+//fontSize: 22.0,
+//),
+//),
+//],
+//),
